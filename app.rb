52ff4e3f9005c14e4c98011c33f80f8854c602cc
myrtokonst <myrtokonst@gmail.com>
2019-04-01 15:01:02 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Myrto"
+  end
+
+
+  get '/hometown' do
+    "My hometown is Athens"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Royal Blue"
+  
+  end
+
 end
